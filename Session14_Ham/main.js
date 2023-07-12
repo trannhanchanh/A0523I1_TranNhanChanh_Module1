4b1ffaed544ea17882e3e4b5f332bee2e5731ff8
trannhanchanh <chanhtnde160156@fpt.edu.vn>
2023-07-12 18:46:03 +0700

    message

---
+++ Session14_Ham/main.js
@@ -0,0 +1,10 @@
+function square(number){
+    var result= number*number;
+    return result;
+}
+function calculate(){
+    var input= document.getElementById("input").value;
+    var result= square(input);
+    document.getElementById("result").innerHTML= result;
+
+}
